cac1c7bc75f9c9a09146124317fc6dfd933e624c
hcs4125 <hcs4125@gmail.com>
2022-03-13 11:48:47 +0900

    update:Selection Sorting

--- src/Edu.java
+++ src/Edu.java
@@ -1,17 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Edu {
     public static void main(String[] args) {
-        int x = 1234;
-        String str = "1234";
+        int[] ary = {32, 11, 2, 5, 78, 32};
+        solution(ary);
+        System.out.println(Arrays.toString(ary));
+    }
 
-        int y = 1;
-        System.out.println("===================");
-        System.out.println(x + y);
-        System.out.println(str + y);
-        System.out.println("===================");
-        System.out.println(x);//변수값
-        System.out.println("x");
-        System.out.println(str);
+    private static  void solution(int[] ary) {
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = i + 1; j < ary.length; j++) {
+                if (ary[i] > ary[j]) {
+                    int tmp = ary[i];
+                    ary[i] = ary[j];
+                    ary[j] = tmp;
+                }
+            }
+        }
     }
 }
